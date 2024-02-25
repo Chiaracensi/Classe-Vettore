5eecc6d558a2b9efc42bd9b5975cd5bb193e07fe
Utente <Utente@DESKTOP-JO50B91>
2024-02-25 20:22:25 +0100

    Aggiungere i file di progetto.

---
+++ Classe Vettore/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Vettore 
+{
+    public class Vettore{
+    public double x { get; set; }
+    public double y { get; set; }
+    public Vettore(double X, double Y)
+    {
+        X = x;
+        Y = y;
+
+    }
+    public static Vettore operator -(Vettore v1, Vettore v2)
+    {
+        return new Vettore(v1.x - v2.x, v1.y - v2.y);
+    }
+    public static Vettore operator *(Vettore v1, Vettore v2)
+    {
+        return new Vettore(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    public static Vettore operator *(Vettore v1, int n)
+    {
+        return new Vettore(v1.x * n, v1.y * n);
+    }
+
+    public static Vettore operator *(int n, Vettore v1)
+    {
+        return new Vettore(v1.x * n, v1.y * n);
+    }
+
+    public static Vettore operator /(Vettore v1, int n)
+    {
+        return new Vettore(v1.x / n, v1.y / n);
+    }
+
+    public static Vettore operator +(Vettore v1)
+    {
+        return new Vettore(v1.x, v1.y);
+    }
+    public static Vettore operator -(Vettore v1)
+    {
+        return new Vettore(-v1.x, -v1.y);
+    }
+    public static bool operator ==(Vettore v1, Vettore v2)
+    {
+        if (v1.x == v2.x && v1.y == v2.y)
+            return true;
+        else
+            return false;
+    }
+    public static bool operator !=(Vettore v1, Vettore v2)
+    {
+        if (v1.x != v2.x)
+            return true;
+        else
+            return false;
+    }     
+}
+}
---
+++ Classe Vettore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Vettore 
+{
+    internal class Vettore
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
